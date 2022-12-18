11fcaefba04a39c5b3086762dbda29f59a0f4ae4
jyp1763 <108202344+jyp1763@users.noreply.github.com>
2022-12-17 19:58:45 -0500

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,35 @@
+var questions = [
+    {
+      title: 'Which option is the correct format for a title tag?',
+      choices: ['<title></title>', '</title><title>', '<title>>/title>', '<<title>><</title>>'],
+      answer: '<title></title>',
+    },
+    {
+      title: 'which data type in JavaScript is defined by true of false values?',
+      choices: ['Number', 'string', 'boolean', 'undefined'],
+      answer: 'boolean',
+    },
+    {
+        title:
+          'String values must be enclosed within ____ when being assigned to variables.',
+        choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+        answer: 'quotes',
+    },
+    {
+      title: 'Arrays in JavaScript can be used to store ____.',
+      choices: [
+        'numbers and strings',
+        'other arrays',
+        'booleans',
+        'all of the above',
+      ],
+      answer: 'all of the above',
+    },
+    {
+      title:
+        'A very useful tool used during development and debugging for printing content to the debugger is:',
+      choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+      answer: 'console.log',
+    },
+  ];
+  
